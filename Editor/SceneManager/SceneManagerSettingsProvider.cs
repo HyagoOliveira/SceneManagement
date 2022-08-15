f91e29a74f0e5cff94b51221fd2ff2cfea6955de
Hyago Oliveira <hyagogow@gmail.com>
2022-08-15 16:04:08 -0300

    Force to call Awake when in Editor

--- Editor/SceneManager/SceneManagerSettingsProvider.cs
+++ Editor/SceneManager/SceneManagerSettingsProvider.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEditor;
 using ActionCode.ScriptableSettingsProvider.Editor;
 
@@ -6,15 +7,23 @@ namespace ActionCode.SceneManagement.Editor
     public sealed class SceneManagerSettingsProvider :
         AbstractScriptableSettingsProvider<SceneManagerSettings>
     {
+        private const string CONFIG_NAME = "com.actioncode.scene-management";
+
         public SceneManagerSettingsProvider() :
             base("ActionCode/Scene Manager")
         { }
 
-        protected override string GetConfigName() =>
-            "com.actioncode.scene-management";
+        protected override string GetConfigName() => CONFIG_NAME;
 
         [SettingsProvider]
         private static SettingsProvider CreateProjectSettingsMenu() =>
             new SceneManagerSettingsProvider();
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void AwakeSettings()
+        {
+            var hasSettings = EditorBuildSettings.TryGetConfigObject(CONFIG_NAME, out SceneManagerSettings settings);
+            if (hasSettings) settings.Awake();
+        }
     }
 }

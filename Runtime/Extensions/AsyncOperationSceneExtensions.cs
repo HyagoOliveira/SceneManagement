53fa141f25e7358236efeb855af2026640508743
Hyago Oliveira <hyagogow@gmail.com>
2022-08-15 19:07:59 -0300

    Refactor extensions to return IEnumerator

--- Runtime/Extensions/AsyncOperationSceneExtensions.cs
+++ Runtime/Extensions/AsyncOperationSceneExtensions.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections;
 using UnityEngine;
-using System.Threading.Tasks;
 
 namespace ActionCode.SceneManagement
 {
@@ -13,27 +13,16 @@ namespace ActionCode.SceneManagement
         /// Awaits until the operation reaches its activation progress.
         /// </summary>
         /// <param name="progress">An implementation to report progress.</param>
-        /// <returns>An operation that will be completed when it reaches its activation progress.</returns>
-        public static async Task WaitUntilActivationProgress(this AsyncOperation operation, IProgress<float> progress = null)
+        /// <returns>An enumerator that will wait until the operation reaches its activation progress.</returns>
+        public static IEnumerator WaitUntilActivationProgress(this AsyncOperation operation, IProgress<float> progress = null)
         {
             // The real loading progress will be between 0F to 0.9F until the new scene is activated.
             const float activationProgress = 0.9F;
             while (operation.progress < activationProgress)
             {
                 progress?.Report(operation.progress + 0.1F);
-                await Task.Yield();
+                yield return null;
             }
-        }
-
-        /// <summary>
-        /// Awaits until the operation is marked as done.
-        /// </summary>
-        /// <param name="operation"></param>
-        /// <returns>An operation that will be completed when it is done.</returns>
-        public static async Task WaitUntilIsDone(this AsyncOperation operation)
-        {
-            while (!operation.isDone)
-                await Task.Yield();
         }
     }
 }

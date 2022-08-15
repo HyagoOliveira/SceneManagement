9025bd7245e069b658a40c1fd7430ecb0fd8db1e
Hyago Oliveira <hyagogow@gmail.com>
2022-08-14 21:59:14 -0300

    Refactor LoadingText to show progression

---
+++ Runtime/UI/LoadingText.cs
@@ -0,0 +1,28 @@
+#if UI_MODULE
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ActionCode.SceneManagement
+{
+    /// <summary>
+    /// Updates a local <see cref="Text"/> with the current loading progress.
+    /// </summary>
+    [RequireComponent(typeof(Text))]
+    public sealed class LoadingText : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The local Text component.")]
+        private Text text;
+        [SerializeField, Tooltip("The Scene Manager settings.")]
+        private SceneManagerSettings settings;
+
+        public const string TEXT_FORMAT = "{0} %";
+
+        private void Reset() => text = GetComponent<Text>();
+        private void OnEnable() => settings.OnProgressChanged += HandleProgressChanged;
+        private void OnDisable() => settings.OnProgressChanged -= HandleProgressChanged;
+
+        private void HandleProgressChanged(float progress) =>
+            text.text = string.Format(TEXT_FORMAT, progress);
+    }
+}
+#endif

3884d517ba8a956c96a00c9e68fd8b740d3a3e25
Hyago Oliveira <hyagogow@gmail.com>
2022-08-15 19:07:28 -0300

    Add internal SceneManagerBehaviour

---
+++ Runtime/SceneManager/SceneManagerBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ActionCode.SceneManagement
+{
+    /// <summary>
+    /// Internal component to execute coroutines inside <see cref="SceneManagerSettings"/>.
+    /// </summary>
+    [DisallowMultipleComponent]
+    internal sealed class SceneManagerBehaviour : MonoBehaviour
+    {
+    }
+}

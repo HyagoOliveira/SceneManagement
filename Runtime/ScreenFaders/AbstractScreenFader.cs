bee15d3e07219b310f0e67c469f6b20046d51fb3
Hyago Oliveira <hyagogow@gmail.com>
2022-08-16 17:25:26 -0300

    AbstractScreenFader implements IScreenFader

--- Runtime/ScreenFaders/AbstractScreenFader.cs
+++ Runtime/ScreenFaders/AbstractScreenFader.cs
@@ -4,21 +4,12 @@ using System.Collections;
 namespace ActionCode.SceneManagement
 {
     /// <summary>
-    /// Abstract component used on objects able to fade the screen in and out.
+    /// Abstract component able to fade the screen in and out.
     /// </summary>
     [RequireComponent(typeof(Canvas))]
-    public abstract class AbstractScreenFader : MonoBehaviour
+    public abstract class AbstractScreenFader : MonoBehaviour, IScreenFader
     {
-        /// <summary>
-        /// Fades the screen in (the content will appear).
-        /// </summary>
-        /// <returns>An enumerator that will be completed when all screen content appears.</returns>
         public abstract IEnumerator FadeIn();
-
-        /// <summary>
-        /// Fades the screen out (the content will disappear).
-        /// </summary>
-        /// <returns>An enumerator that will be completed when all screen content disappears.</returns>
         public abstract IEnumerator FadeOut();
     }
 }

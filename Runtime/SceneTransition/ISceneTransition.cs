915fb2e13b34663c2ee259f3b9c0beae0cda6527
Hyago Oliveira <hyagogow@gmail.com>
2022-08-31 22:34:52 -0300

    Add SceneTransition

---
+++ Runtime/SceneTransition/ISceneTransition.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ActionCode.SceneManagement
+{
+    public interface ISceneTransition
+    {
+        /// <summary>
+        /// Action fired when the Scene Loading Progress changes.
+        /// <para>
+        /// The given float param is the progress in percentage, 
+        /// i.e. a number between 0F and 100F.
+        /// </para>
+        /// </summary>
+        event Action<float> OnProgressChanged;
+
+        /// <summary>
+        /// Whether a Scene Loading process is happening. 
+        /// </summary>
+        bool IsLoading { get; }
+
+        Task LoadScene(string scene, ISceneTransitionData data);
+    }
+}
---
+++ Runtime/SceneTransition/SceneTransition.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using ActionCode.AwaitableCoroutines;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ActionCode.SceneManagement
+{
+    public sealed class SceneTransition : ISceneTransition
+    {
+        public event Action<float> OnProgressChanged;
+
+        public bool IsLoading { get; private set; }
+
+        public async Task LoadScene(string scene, ISceneTransitionData data)
+        {
+            try
+            {
+                await AwaitableCoroutine.Run(LoadSceneCoroutine(scene, data));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private IEnumerator LoadSceneCoroutine(string scene, ISceneTransitionData data)
+        {
+            if (IsLoading)
+                throw new Exception($"Cannot load {scene} since other scene is being loaded.");
+
+            IsLoading = true;
+            var hasLoadingScene = !string.IsNullOrEmpty(data.LoadingScene);
+
+            yield return data.ScreenFader?.FadeOut();
+            IProgress<float> progress = new Progress<float>(ReportProgress);
+
+            if (hasLoadingScene)
+            {
+                // will automatically unload the previous Scene.
+                var loadingSceneOperation = SceneManager.LoadSceneAsync(data.LoadingScene, LoadSceneMode.Single);
+                yield return loadingSceneOperation;
+
+                progress.Report(0F);
+                yield return data.ScreenFader?.FadeIn();
+            }
+
+            yield return new WaitForSeconds(data.TimeBeforeLoading);
+
+            var loadingOperation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+            if (loadingOperation == null) yield break;
+
+            // will prevent to automatically unload the data.LoadingScene.
+            loadingOperation.allowSceneActivation = false;
+
+            yield return loadingOperation.WaitUntilActivationProgress(progress);
+
+            progress.Report(1F);
+            yield return new WaitForSeconds(data.TimeAfterLoading);
+
+            if (hasLoadingScene) yield return data.ScreenFader?.FadeOut();
+
+            // will automatically unload the data.LoadingScene.
+            loadingOperation.allowSceneActivation = true;
+            yield return data.ScreenFader?.FadeIn();
+
+            IsLoading = false;
+        }
+
+        private void ReportProgress(float progress) => OnProgressChanged?.Invoke(progress * 100F);
+    }
+}

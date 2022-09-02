fb2b635e2929ecd00e28930b8292eb77f538f084
Hyago Oliveira <hyagogow@gmail.com>
2022-09-02 16:14:41 -0300

    Refactor LoadSceneAfterTime to use LoadScene

--- Runtime/Utils/LoadSceneAfterTime.cs
+++ Runtime/Utils/LoadSceneAfterTime.cs
@@ -5,26 +5,25 @@ namespace ActionCode.SceneManagement
 {
     /// <summary>
     /// This component loads the <see cref="scene"/> after <see cref="time"/>.
-    /// <para>You can customize the transition params by setting <see cref="transition"/>.</para>
+    /// <para>You can customize the transition params by setting the local <see cref="LoadScene"/> component.</para>
     /// </summary>
     [DisallowMultipleComponent]
+    [RequireComponent(typeof(LoadScene))]
     public sealed class LoadSceneAfterTime : MonoBehaviour
     {
-        [Tooltip("The Scene Manager")]
-        public SceneManager sceneManager;
+        [Tooltip("The local LoadScene component.")]
+        public LoadScene loader;
         [Tooltip("The new scene to load."), Scene]
         public string scene;
-        [Tooltip("The scene transition data to use. The default one will be used if none is set.")]
-        public SceneTransition transition;
         [Tooltip("Time (in seconds) to wait before load the new scene."), Min(0F)]
         public float time = 1f;
+
+        private void Reset() => loader = GetComponent<LoadScene>();
 
         private IEnumerator Start()
         {
             yield return new WaitForSeconds(time);
-
-            if (transition) _ = sceneManager.LoadScene(scene, transition);
-            else _ = sceneManager.LoadScene(scene);
+            loader.Load(scene);
         }
     }
 }

3c94c3a370f38e0a169f3e53fa874a5de34dee17
Hyago Oliveira <hyagogow@gmail.com>
2022-09-02 09:09:27 -0300

    Add TextMeshPro support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - AnimationScreenFader component and prefab
 - ScreenFaderPool
 - SceneTransition
+- TextMeshPro support
 
 ### Changed
 - Scriptable Settings Provider for Scene Manager 
--- Runtime/UI/LoadingText.cs
+++ Runtime/UI/LoadingText.cs
@@ -2,25 +2,40 @@ using UnityEngine;
 #if UI_MODULE
 using UnityEngine.UI;
 #endif
+#if TEXT_MESH_PRO_MODULE
+using TMPro;
+#endif
 
 namespace ActionCode.SceneManagement
 {
     /// <summary>
     /// Updates a local <see cref="Text"/> with the current loading progress.
     /// </summary>
-    [RequireComponent(typeof(Text))]
     public sealed class LoadingText : MonoBehaviour
     {
 #if UI_MODULE
         [SerializeField, Tooltip("The local Text component.")]
-        private Text text;
+        private Text textUI;
+#endif
+#if UI_MODULE
+        [SerializeField, Tooltip("The local Text component.")]
+        private TMP_Text textMesh;
 #endif
         [SerializeField, Tooltip("The Scene Manager.")]
         private SceneManager sceneManager;
 
         public const string TEXT_FORMAT = "{0} %";
 
-        private void Reset() => text = GetComponent<Text>();
+        private void Reset()
+        {
+#if UI_MODULE
+            textUI = GetComponent<Text>();
+#endif
+#if UI_MODULE
+            textMesh = GetComponent<TMP_Text>();
+#endif
+        }
+
         private void OnEnable() => sceneManager.OnProgressChanged += HandleProgressChanged;
         private void OnDisable() => sceneManager.OnProgressChanged -= HandleProgressChanged;
 
@@ -29,7 +44,10 @@ namespace ActionCode.SceneManagement
         private void SetText(string text)
         {
 #if UI_MODULE
-            this.text.text = string.Format(TEXT_FORMAT, text);
+            if (textUI) textUI.text = text;
+#endif
+#if UI_MODULE
+            if (textMesh) textMesh.text = text;
 #endif
         }
     }

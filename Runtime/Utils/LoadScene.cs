c1d0f2b7226c104756dcc48603212bb446612120
Hyago Oliveira <hyagogow@gmail.com>
2022-09-03 17:14:39 -0300

    Improve LoadScene component

--- Runtime/Utils/LoadScene.cs
+++ Runtime/Utils/LoadScene.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using System.Collections;
 
 namespace ActionCode.SceneManagement
 {
     /// <summary>
     /// This component was build to quickly load new Scenes. You may use it in Button actions.
-    /// <para>You can customize the transition behaviour by setting <see cref="transition"/> attribute.</para>
+    /// <para>You can customize the transition behaviour by setting <see cref="transition"/> field.</para>
     /// </summary>
     [DisallowMultipleComponent]
     public sealed class LoadScene : MonoBehaviour
@@ -13,15 +14,41 @@ namespace ActionCode.SceneManagement
         public SceneManager sceneManager;
         [Tooltip("The scene transition data to use. The default one will be used if none is set.")]
         public SceneTransition transition;
+        [Tooltip("The new scene to load."), Scene]
+        public string scene;
+        [Tooltip("Time (in seconds) to wait before load the new scene."), Min(0F)]
+        public float time = 0f;
+        [Tooltip("If enabled, the Loading process will happening in Start() function.")]
+        public bool loadOnStart = false;
+
+        private void Start()
+        {
+            if (loadOnStart) Load();
+        }
 
         /// <summary>
-        /// Loads the given scene using <see cref="transition"/>. The default one will be used if none is set.
+        /// Loads the <see cref="scene"/>.
+        /// <para>
+        /// If the local<see cref="transition"/> is not set, 
+        /// the <see cref="SceneManager.defaultTransition"/> will be used instead.
+        /// </para>
+        /// </summary>
+        public void Load() => Load(scene);
+
+        /// <summary>
+        /// Loads the given scene.
+        /// <para><inheritdoc cref="Load"/></para>
         /// </summary>
         /// <param name="scene">A Scene path or name to load.</param>
-        public async void Load(string scene)
+        public void Load(string scene) => StartCoroutine(LoadCoroutine(scene));
+
+        private IEnumerator LoadCoroutine(string scene)
         {
-            if (transition) await sceneManager.LoadScene(scene, transition);
-            else await sceneManager.LoadScene(scene);
+            // Using Coroutine because Task.Delay() is not supported in some Platforms (WebGL).
+            yield return new WaitForSeconds(time);
+
+            if (transition) _ = sceneManager.LoadScene(scene, transition);
+            else _ = sceneManager.LoadScene(scene);
         }
     }
 }
